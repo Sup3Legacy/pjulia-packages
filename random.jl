b5d2f755bd985b3b4a9005e96aae6de03f587e0b
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-03 19:20:47 +0100

    fixed array assignment in random

--- random.jl
+++ random.jl
@@ -1,12 +1,12 @@
-seed = [123456789]
-m = 2 ^ 31
-a = 1103515245
-c = 12345
+random_seed = [123456789]
+random_m = 2 ^ 31
+random_a = 1103515245
+random_c = 12345
 
 """
 randint(a, b) retunr a random integer in [a, b[.
 """
 function randint(min :: Int64, max :: Int64) :: Int64
-    seed[0] = (a * seed + c) % m;
-    return seed[0]
+    random_seed[0] := (random_a * random_seed[0] + random_c) % random_m;
+    return random_seed[0]
 end 
