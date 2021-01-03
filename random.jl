b4e1356496b14368d3089b6187a6ee01d0f7624d
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-03 19:31:08 +0100

    fixed random function

--- random.jl
+++ random.jl
@@ -1,5 +1,5 @@
-random_seed = [123456789]
-random_m = 2 ^ 31
+random_seed = [123456783549]
+random_m = 2 ^ 30
 random_a = 1103515245
 random_c = 12345
 
@@ -7,6 +7,8 @@ random_c = 12345
 randint(a, b) retunr a random integer in [a, b[.
 """
 function randint(min :: Int64, max :: Int64) :: Int64
-    random_seed[0] := (random_a * random_seed[0] + random_c) % random_m;
-    return random_seed[0]
+    value = (random_a * random_seed[0] + random_c) % random_m
+    random_seed[0] := value;
+    value = (value % (max - min)) + min
+    return value
 end 
