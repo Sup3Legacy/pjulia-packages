efd556e7ddd3320b1a9e2dd3dd767981b4e1ccb2
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-04 21:16:52 +0100

    Updated random.jl to comply with the new namespace system

--- random.jl
+++ random.jl
@@ -1,14 +1,14 @@
-random_seed = [123456783549]
-random_m = 2 ^ 30
-random_a = 1103515245
-random_c = 12345
+ seed = [123456783549]
+ m = 2 ^ 30
+ a = 1103515245
+ c = 12345
 
 """
-randint(a, b) retunr a random integer in [a, b[.
+randint(a, b) returns a random integer in [a, b[.
 """
 function randint(min :: Int64, max :: Int64) :: Int64
-    value = (random_a * random_seed[0] + random_c) % random_m
-    random_seed[0] := value;
+    value = ( a *  seed[0] +  c) %  m
+    seed[0] := value;
     value = (value % (max - min)) + min
     return value
 end 
