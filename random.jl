8e3feedd10870ddcab8f8f2666c0960656d08954
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-07 16:35:06 +0100

    fixed dependencies

--- random.jl
+++ random.jl
@@ -1,4 +1,5 @@
- seed = [123456783549]
+ seed = [2332411354113524845226412]
+ N = [2 ** 31]
  m = 2 ^ 31 - 1
  a = 16807
  c = 17
@@ -12,6 +13,12 @@ function randint(min :: Int64, max :: Int64) :: Int64
     value = (value % (max - min)) + min
     return value
 end 
+
+"""Returns a random FP value in [0;1[. /!\\ Experimental"""
+function rand()
+    val = div(randint(0, 2 ^ 31) * (2 ^ 31) + randint(0, 2 ^ 31)), (2 ** 63) - 1)
+    return val
+end
 
 function init_seed()
     println("Please enter a new value for the seed")
