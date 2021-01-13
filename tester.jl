d4a31f34ac9a8e2b6726da3457c1115642a95001
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-01-13 18:39:29 +0100

    tester.jl

--- tester.jl
+++ tester.jl
@@ -6,8 +6,12 @@ function testEgal(a::Array,b::Array)
 	return true
 end
 
-assert 1==1 && !(2==1) && 1!=2 && (not 1!=1)
-assert 1==true && !(2==true) && 1!=false && (not 1!=false)
+function testString(a::String) true end
+function testString(a::Array) false end
+function testString(a::Any) false end
+
+assert (1==1) && !(2==1) && (1!=2) && !(1!=1)
+assert 1==true && !(2==true) && 1!=false && (1!=false)
 
 assert 3 < 4 && !(3 < 3) && !(4 < 3)
 assert 3. < 4 && !(3. < 3) && !(4. < 3)
@@ -23,22 +27,33 @@ assert 3. <= 4. && (3. <= 3.) && !(4. <= 3.)
 assert (1 <= true) && !(true <= 0) && (true <= 1) && !(2 <= true)
 assert !(1 <= false) && !(false <= -1) && (false <= 0) && (0 <= false)
 
-assert 3 > 4 && !(3 > 3) && !(4 > 3)
-assert 3. > 4 && !(3. > 3) && !(4. > 3)
-assert 3 > 4. && !(3 > 3.) && !(4 > 3.)
-assert 3. > 4. && !(3. > 3.) && !(4. > 3.)
-assert (0 > true) && (true > 2) && !(true > 1) && !(1 > true)
-assert (-1 > false) && (false > 1) && !(false > 0) && !(0 > false)
+assert !(3 > 4) && !(3 > 3) && (4 > 3)
+assert !(3. > 4) && !(3. > 3) && (4. > 3)
+assert !(3 > 4.) && !(3 > 3.) && (4 > 3.)
+assert !(3. > 4.) && !(3. > 3.) && (4. > 3.)
+assert (2 > true) && (true > 0) && !(true > 1) && !(1 > true)
+assert (1 > false) && (false > -1) && !(false > 0) && !(0 > false)
 
-assert 3 >= 4 && (3 >= 3) && !(4 <= 3)
-assert 3. >= 4 && (3. >= 3) && !(4. >= 3)
-assert 3 >= 4. && (3 >= 3.) && !(4 >= 3.)
-assert 3. >= 4. && (3. >= 3.) && !(4. >= 3.)
-assert (1 >= true) && !(true >= 0) && (true >= 1) && !(2 >= true)
-assert !(1 >= false) && !(false >= -1) && (false >= 0) && (0 >= false)
+assert !(3 >= 4) && (3 >= 3) && (4 >= 3)
+assert !(3. >= 4) && (3. >= 3) && (4. >= 3)
+assert !(3 >= 4.) && (3 >= 3.) && (4 >= 3.)
+assert !(3. >= 4.) && (3. >= 3.) && (4. >= 3.)
+assert (1 >= true) && (true >= 1) && !(true >= 2) && !(0 >= true)
+assert !(-1 >= false) && !(false >= 1) && (false >= 0) && (0 >= false)
 
 
 assert testEgal([1,2,3],[1,2,3])
 assert !testEgal([1,2,3],[1,3,2])
+
+a = "Je suis samuel"
+b = "je suis Samuel"
+
+assert testString(a)
+assert !testString([1,2,3])
+assert !testEgal(a,b)
+
+a[8] = 'S'
+b[0] = a[0]
+assert testEgal(a,b)
 
 println("Success")
