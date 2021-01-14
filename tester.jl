5a7780348e55003b2e3bcd9b7ecbd11f3a428aba
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-01-14 09:18:29 +0100

    rajoute concat à test

--- tester.jl
+++ tester.jl
@@ -11,8 +11,8 @@ function testString(a::Array) false end
 function testString(a::Any) false end
 
 assert (1==1) && !(2==1) && (1!=2) && !(1!=1)
-assert 1==true && !(2==true) && 1!=false && (1!=false)
-
+assert !(2==true) && 1!=false && (1!=false) && 1==true
+	
 assert 3 < 4 && !(3 < 3) && !(4 < 3)
 assert 3. < 4 && !(3. < 3) && !(4. < 3)
 assert 3 < 4. && !(3 < 3.) && !(4 < 3.)
@@ -41,6 +41,8 @@ assert !(3. >= 4.) && (3. >= 3.) && (4. >= 3.)
 assert (1 >= true) && (true >= 1) && !(true >= 2) && !(0 >= true)
 assert !(-1 >= false) && !(false >= 1) && (false >= 0) && (0 >= false)
 
+assert int(3)==3 && int(false)== 0 && int(true)==1 && int(1.1) == 1;
+assert int(-3.1) == -3;
 
 assert testEgal([1,2,3],[1,2,3])
 assert !testEgal([1,2,3],[1,3,2])
@@ -55,5 +57,14 @@ assert !testEgal(a,b)
 a[8] = 'S'
 b[0] = a[0]
 assert testEgal(a,b)
+
+a = "je suis"
+b = "Samuel"
+c = "je suis Samuel"
+
+assert testEgal(a@" "@b,c)
+assert !testEgal(a@b,c)
+asssert testEgal([true,true]@[false,true],[true,true,false,true])
+assert !testEgal([1,3,5]@[3,2],[1,3,3,5,2])
 
 println("Success")
